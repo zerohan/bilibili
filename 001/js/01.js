6e1fb070ee14b556aa6cd62b827ccd8ddf7ceabc
zeroling <2802208829@qq.com>
2015-02-21 21:12:32 +0800

    01
    01

---
+++ 001/js/01.js
@@ -0,0 +1,11 @@
+function clickNextPage1(){ 
+            window.location.href='setup.html';  
+      
+        }  
+ document.getElementById('setup').addEventListener("click", clickNextPage1);  
+ function clickNextPage2(){  
+            window.location.href='setin.html';  
+      
+        }  
+ document.getElementById('setin').addEventListener("click", clickNextPage2); 
+ 
---
+++ 001/js/02.js
@@ -0,0 +1,5 @@
+function clickNextPage3(){  
+            window.location.href='bilibili.html';  
+      
+        }  
+ document.getElementById('most').addEventListener("click", clickNextPage3);   
